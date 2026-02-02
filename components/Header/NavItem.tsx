1b64267c74c0f385c43b6c4cb3f1c0e6cb695516
QuangHung34 <quanghung34wxw@gmail.com>
2026-02-02 15:18:12 +0700

    Update NavItem.tsx

--- components/Header/NavItem.tsx
+++ components/Header/NavItem.tsx
@@ -16,9 +16,8 @@ export const NavItem = ({
     <li className="cursor-pointer">
       <Link
         href={href}
-        className={`${ibmPlexMono.className} text-xs font-medium cursor-pointer`}
+        className={`${ibmPlexMono.className} text-base font-medium cursor-pointer`}
       >
-        {/* thêm class group ở đây */}
         <div className="group relative flex items-center px-4 h-11 rounded-lg cursor-pointer top-nav-item hover:gap-1 leading-[120%] tracking-normal">
           <Image
             src={iconPath}
@@ -27,8 +26,7 @@ export const NavItem = ({
             height={20}
             className="top-nav-item-icon group-hover:inline"
           />
-          {/* text xs bình thường, khi hover thì lên text-xl (20px) */}
-          <p className="group-hover:text-xl transition-all duration-150 group-hover:pr-4">
+          <p className="transition-all duration-150 group-hover:pr-4">
             {label}
           </p>
         </div>
